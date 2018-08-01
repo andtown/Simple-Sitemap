45f9719026462fcb73522353384711fa36cfee1a
andtown <anthonius.andreas@gmail.com>
2018-08-02 00:26:15 +0700

    plugin skeleton creation

---
+++ Simple-Sitemap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Simple Sitemap
+* Plugin URI: 
+* Version: 0.1.0
+* Author: Andtown
+* Author URI: 
+* Description: This is a simple sitemap plugin, It's used to facilitate the search engine bots for site crawling purposes.
+* License: GPL 3
+*/
