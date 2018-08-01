f66bf41114ce6be05da6ccdf8782da09f2cb1492
andtown <anthonius.andreas@gmail.com>
2018-08-02 00:45:04 +0700

    add the simple sitemap main class including its fundamental methods

--- includes/class-simple-sitemap.php
+++ includes/class-simple-sitemap.php
@@ -1,1 +1,79 @@
 <?php
+/**
+ * Simple Sitemap Base Class
+ *
+ * @since 0.1.0
+ */
+
+class Simple_Sitemap {
+
+	/**
+	 * @var object
+	 *
+	 * @since 0.1.0
+	 */
+	protected static $instance;
+
+
+	/**
+	 * Flush rewrite rules
+	 *
+	 * @since 0.1.0
+	 */
+	public static function flush_rewrite_rules() {
+		flush_rewrite_rules();
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return object 
+	 */
+	public static function get_instance() {
+        if ( !static::$instance ) new static;
+        return static::$instance;
+	}
+
+    /**
+     * Class Constructor
+     *
+     * @since 0.1.0
+     */
+	public function __construct() {
+
+		$this->init();
+
+	}
+
+	/**
+	 * 
+	 *
+	 * Initializing class properties
+	 *
+	 * @since 0.1.0
+	 */
+	protected function init() {
+		static::$instance = $this;
+	}
+
+	/**
+	 * 
+	 *
+	 * @since 0.1.0
+	 */
+	public static function activate_plugin() {			
+		static::flush_rewrite_rules();
+	}
+
+	/**
+	 * 
+	 *
+	 * @since 0.1.0
+	 */
+	public static function deactivate_plugin() {
+		static::flush_rewrite_rules();
+	}
+
+}
