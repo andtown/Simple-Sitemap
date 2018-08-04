ed4e4afd0efaf3e9c258c9892e45eeeac60a71f6
andtown <anthonius.andreas@gmail.com>
2018-08-05 02:48:10 +0700

    New build

--- Simple-Sitemap.php
+++ Simple-Sitemap.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Simple Sitemap
 * Plugin URI: 
-* Version: 0.1.9
+* Version: 0.2.1
 * Author: Andtown
 * Author URI: 
 * Description: This is a simple sitemap plugin, It's used to facilitate the search engine bots for site crawling purposes.
